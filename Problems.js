97f9743093255f89d9877545e2306018a6ad22c1
Inna Nekhai <nekhai.inna@gmail.com>
2017-10-21 18:29:12 +0300

    Homework_4

---
+++ Problems.js
@@ -0,0 +1,24 @@
+var c=15, d=2;
+result=c+d;
+alert(result);
+
+var a=10, b=2, c=5;
+alert(a+b+c);
+
+var a=17, b=10;
+c=a-b;
+var d=7;
+result=c+d;
+alert(result);
+
+var str="Привет, Мир!";
+alert(str);
+
+var str1="Привет, ", str2="Мир!";
+alert(str1+str2);
+
+var name="Инна";
+alert("Привет, "+name+"!");
+
+var age=27;
+alert("Мне "+age+"лет!");
