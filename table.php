dc607dd1561d465a708cd9016192bd3f26c6a25e
Inna Nekhai <nekhai.inna@gmail.com>
2017-11-13 01:49:51 +0200

    homework_11

--- table.php
+++ table.php
@@ -7,27 +7,50 @@
 <head>
     <meta charset="UTF-8">
     <title>Список пользователей</title>
+    <style>
+        td, th{
+            border-style: solid;
+            border-width: 1px;
+            border-color: lightgrey;
+            padding: 10px;
+        }
+        th{
+            font-style: italic;
+            color: gray;
+        }
+    </style>
 </head>
 <body>
 
 <table>
     <tr>
+        <th>№</th>
         <th>Имя</th>
         <th>Фамилия</th>
         <th>Email</th>
         <th>Телефон</th>
     </tr>
     <?php
-    $arrdate = file('user.txt');
- /*   foreach ($arrdate as $elem){
-        $elem = explode(' ', $elem);
-    }
-var_dump($arrdate);*/
-    echo $arrdate[1];
-    ?>
+    $arrdata = file('user.txt'); //$arrdata - массив с инф. о пользователях, размещенной в строку
+
+    foreach ($arrdata as $elem) {
+        $elem = explode("\t", $elem);
+        $data[]=$elem; //$data - двухмерный массив, инф. о каждом пользователе отражается в массиве
+    }?>
+
+
+    <?php foreach ($data as $key=>$elem){?>
+        <tr>
+        <td><?php print_r ($key+ 1);?></td>
+        <?php foreach ($elem as $key2=>$elem1 ){?>
+            <?php if ($key2<=3){ ?>
+        <td><?php print_r($elem1 .PHP_EOL); ?></td>
+            <?php } ?>
+        <?php } ?>
+    </tr>
+    <?php } ?>
 
 </table>
-
 
 </body>
 </html>
